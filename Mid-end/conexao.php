66545987b4127358cfe32d9c8d4748d85f833071
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-05-09 22:08:06 -0300

    Delete conexao.php

--- Mid-end/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "sistema_de_reserva";
-$conexao =@mysql_connect($host,$user, $pass) or die(mysql_error());
-mysql_select_db($db) or die(mysql_error());
-?>
