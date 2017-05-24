f6386b1762988375e43bfe7549a8c9885c939729
1Manifesto <lcsvncs429@gmail.com>
2017-05-24 05:19:57 -0300

    É isso ai

---
+++ Front-end/homePage/usuarioVisitante/js/freelancer.js
@@ -0,0 +1,239 @@
+// Freelancer Theme JavaScript
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function(){ 
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Floating label headings for the contact form
+    $(function() {
+        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function() {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function() {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
+    });
+
+})(jQuery); // End of use strict
+
+/*                                  LOGIN                       */
+function toggleNavPanel(x)
+{
+    var panel = document.getElementById(x), navarrow = document.getElementById("navarrow"), maxH="600px";
+    if(panel.style.height == maxH){
+        panel.style.height = "0px";
+        navarrow.innerHTML = "&#9662;";
+        document.getElementById('logBar').style.background = "#2C3E50";
+    }else{
+        panel.style.height = maxH;
+        navarrow.innerHTML = "&#9652;";
+        document.getElementById('logBar').style.background = "#18BC9C";
+        document.getElementById('logBar').style.color = 'white';
+    }
+}
+/*                          MODULO DE LOGIN                                 */
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+      if (e.type === 'keyup') {
+            if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+        if( $this.val() === '' ) {
+            label.removeClass('active highlight'); 
+            } else {
+            label.removeClass('highlight');   
+            }   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+            label.removeClass('highlight'); 
+            } 
+      else if( $this.val() !== '' ) {
+            label.addClass('highlight');
+            }
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
+function date()
+{
+    var now = new Date;
+    var day = now.getDate();
+    var month = now.getMonth();
+    var year = now.getFullYear();
+    month++;
+    if(day < 10)
+    {
+        var zero = '0';
+        day = zero.concat(day);
+    }
+    if(month < 10)
+    {
+        var zero = '0';
+        month = zero.concat(month);
+    }
+    var d = new Date();
+     days = ["Domingo","Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sábado"];
+    return days[d.getDay()] + ', ' + day + '/' + month + '/'+ year;
+}   
+function Status(status) 
+{
+    for(var i = 0; i < 150;i++)
+    {
+        var id_str = "q";
+        id_str = id_str.concat(100+i);
+        if(status[i] == 0)
+            var x;
+        else if(status[i] == 1)
+        {
+                document.getElementById(id_str).style.background = "#F0F03E";
+        }
+        else if(status[i] == 2)
+        {
+            document.getElementById(id_str).style.background = "#E03A3A";
+            document.getElementById(id_str).style.cursor = "auto";
+        }
+
+    }
+}
+function setDate(x)
+{
+            $('#datepicker').on('changeDate', function() {
+                $('#my_hidden_input').val($('#datepicker').datepicker('getFormattedDate'));
+             });
+
+            var semana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
+            var data = document.getElementById("my_hidden_input").value;
+            var arr = data.split("/").reverse();
+            var teste = new Date(arr[0], arr[1] - 1, arr[2]);
+            var dia = teste.getDay();
+            if(x == 'inicio')
+            {
+                document.getElementById("Title").innerHTML = date();
+                //requisita CHAR de status da tabela
+
+
+
+                var status = [0,2,1,2,2,2,2,0,0,2,2,1,0,2,1,2,1,2,2,1,2,1,2,2,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+                2,2,2,2,0,0,2,0,2,0,2,2,2,2,2,2,2,0,2,0,2,2,2,2,0,0,0,0,2,2,2,1,2,2,2,2,0,0,1,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+                2,2,2,2,2,2,0,2,0,2,2,0,2,2,2,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,2,2,2,2,0,2,1];
+                Status(status);
+            }
+            else
+            {
+                document.getElementById("Title").innerHTML = semana[dia] + ', ' + data;
+                //requisita CHAR de status da tabela
+                var status = [1,2,1,2,2,2,2,0,0,2,2,1,0,2,1,2,1,2,2,1,2,1,2,2,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+                2,2,2,2,0,0,2,0,2,0,2,2,2,2,2,2,2,0,2,0,2,2,2,2,0,0,0,0,2,2,2,1,2,2,2,2,0,0,1,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+                2,2,2,2,2,2,0,2,0,2,2,0,2,2,2,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,2,2,2,2,0,2,1];
+                Status(status);
+            }
+            
+}
+function init()
+{
+        /*                  IMAGEM              */
+        // Get the modal
+        var modal = document.getElementById('myModal');
+
+        // Get the image and insert it inside the modal - use its "alt" text as a caption
+        var img = document.getElementById('myImg');
+        var modalImg = document.getElementById("img01");
+        var captionText = document.getElementById("caption");
+        img.onclick = function(){
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+
+        /*              CALENDARIO              */
+        setDate('inicio');
+        $('#datapicker').datepicker({
+        format: "dd/mm/yyyy",
+        maxViewMode: 3,
+        language: "pt-BR",
+        keyboardNavigation: true,
+        forceParse: true,
+        todayHighlight: true
+        }).on('changeDate', function (e) {
+        setDate();
+        });
+}
+function showImage(x)
+{
+    if(x == 'FT')
+        document.getElementById("myImg").src = "img/mapaFT.png";
+    else if(x == 'SG')
+        document.getElementById("myImg").src = "img/mapaSG.png";
+
+            /*                  IMAGEM              */
+    // Get the modal
+    var modal = document.getElementById('myModal');
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById('myImg');
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+}
+function initMap() {
+        var uluru = {lng: -47.872535, lat: -15.763555};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+    });
+}
