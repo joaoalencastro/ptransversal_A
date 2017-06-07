7ec9b47069be926ba09c7e08cfe8f1733e73d27f
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-06-07 20:59:08 -0300

    Update conexao.php

--- Mid-end/conexao.php
+++ Mid-end/conexao.php
@@ -5,6 +5,9 @@ $pass = "senhadobanco";
 $db = "sistemareservadodb";
 $conexao = new mysqli ($host, $user, $pass, $db);
 
-// Caso algo tenha dado errado, exibe uma mensagem de erro
-if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
+// Caso a conexo tenha dado errado, exibe uma mensagem de erro.
+if (mysqli_connect_errno()) {
+    die('Não foi possível conectar-se ao banco de dados: ' . mysqli_connect_error());
+    exit();
+}
 ?>
