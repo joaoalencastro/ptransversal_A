d2b58a25c5c243bb632e00b06201e51d65727bf0
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-06-10 11:57:30 -0300

    Front-End Files

---
+++ Front-end/homePage/php/recuperar_senha.php
@@ -0,0 +1,21 @@
+<?php
+  require ('conexao.php');
+?>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <title>Sistema de recuperação de senha</title>
+  </head>
+  <body>
+    <form action="" method="post">
+      <input type="email" name="email" placeholder="Digite seu E-mail cadastrado" require style="width: 320px; padding: 8px;"><br><br>
+      <input type="submit" name="Enviar" value="Recuperar">
+    </form>
+
+    <?php
+
+    ?>
+
+  </body>
+</html>
