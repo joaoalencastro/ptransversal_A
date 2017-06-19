1b6dcf824f246f86ef753f65d052ce7a426a0d25
Bruno Vieira Dutra <bruno.dutra@uiot.org>
2017-06-19 02:51:13 -0300

    Update User_authentication.py

--- User_Authentication/User_authentication.py
+++ User_Authentication/User_authentication.py
@@ -1,17 +1,34 @@
 import urllib
 import urllib2
 import webbrowser
-
 import re
+import popular
+
+
+tabela    = popular.Banco()
+email     = tabela.PegaEmailDigitado()[0][0]
+rg        = tabela.PegaEmailDigitado()[0][1]
+nome      = tabela.PegaEmailDigitado()[0][2]
+matricula = tabela.PegaEmailDigitado()[0][3]
+data1      = tabela.PegaEmailDigitado()[0][4]
+
 
 url = "https://aluno.unb.br/alunoweb/default/sca/solicitarsenha"
-data = urllib.urlencode({'nome': '*****', '***': '***', '***': '**','**': '**/**/****'})
+data = urllib.urlencode({'nome': nome, 'matricula': matricula, 'identidade': rg,'data_nascimento': data1})
 results = urllib2.urlopen(url, data)
 conteudo_html = str(results.read())
 
 email_unb = re.search(r'<b>E-mail:</b> (.*)</label>', conteudo_html).group(1)
 email_alternativo = re.search(r'E-mail alternativo:</b>(.*)</label>', conteudo_html).group(1)
 
-print "Email UnB \t\t\t Email Alternativo"
-print email_unb+" \t\t"+email_alternativo
+email = " " + email
 
+
+if (email == email_alternativo):
+	print ("Gravado 1!")
+	tabela.Inserir(email, rg, nome, matricula, data1, 1)
+	tabela.ApagaTabela_transitoria()
+else:
+  print ("Gravado 0!")
+	tabela.Inserir(email, rg, nome, matricula, data1, 0)
+	tabela.ApagaTabela_transitoria()
