5192f0c1c5837b77ea8de02f79e95a72164c966b
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-06-20 17:54:41 -0300

    Create cadastrar_definitivo.php

---
+++ Mid-end/cadastro_user/cadastrar_definitivo.php
@@ -0,0 +1,62 @@
+<html  = lang"pt-br">
+<head>
+<title>Cadastrando...</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<script type='text/javascript'>
+
+		function index(){
+				setTimeout("window.location='index.html'",0003);
+		}
+
+		</script>
+
+</head>
+<body>
+<?php
+
+session_start();
+
+require('../conexao/conexao.php');
+
+$matricula=$_SESSION['email'];
+
+$sql = "SELECT * FROM usuariodef WHERE matricula=$matricula";
+
+$result = mysqli_query($conexao,$sql);
+if (!$result) {
+  die('Algo deu errado na conexão. Erro: ' . mysqli_error($conexao));
+}
+
+while ($ln = mysqli_fetch_array($result)){
+
+$nome      = $ln['nome'];
+$email     = $ln['email'];
+$matricula = $ln['matricula'];
+$datanascimento = $ln['datanascimento'];
+$tipo = 1;
+$rg        = $ln['rg'];
+$senha     = $ln['senha'];
+$autenticado = $ln['autenticado'];
+  if (!$autenticado) {
+      echo"<script>alert('Dados Inválidos. Tente novamente.');</script>";
+  } else {
+    	$sql = "INSERT INTO usuario(nome, email, matricula, datanascimento, tipo, rg, senha)
+    	VALUES('$nome', '$email', '$matricula', '$datanascimento', '$tipo', '$rg', '$senha')";
+    	$result = mysqli_query($conexao,$sql);
+
+      if (!$result) {
+    		die('Algo deu errado. Erro: ' . mysqli_error($conexao));
+    	}
+
+    	echo "<center><h1>Usuaŕio cadastrado com sucesso!</h1></center>";
+      session_destroy();
+    	echo "<script>index()</script>";
+    }
+}
+
+?>
+
+<a href="javascript:window.history.go(-1)">Voltar</a>
+
+</body>
+</html>
