904c1d826d46bbcdd9a9a65f8573cece807c58cc
Bruno Vieira Dutra <bruno.dutra@uiot.org>
2017-06-20 17:22:28 -0300

    Update User_authentication.py

--- User_Authentication/User_authentication.py
+++ User_Authentication/User_authentication.py
@@ -14,11 +14,14 @@ while True:
 	tabela    = popular.Banco()
 	if (tabela.PegaEmailDigitado() != tuple()):	
 		
-		email     = tabela.PegaEmailDigitado()[0][0]
-		rg        = tabela.PegaEmailDigitado()[0][1]
-		nome      = tabela.PegaEmailDigitado()[0][2]
-		matricula = tabela.PegaEmailDigitado()[0][3]
-		data1      = tabela.PegaEmailDigitado()[0][4]
+		nome     	= tabela.PegaEmailDigitado()[0][0]
+		email     	= tabela.PegaEmailDigitado()[0][1]
+		senha       = tabela.PegaEmailDigitado()[0][2]
+		data1      	= tabela.PegaEmailDigitado()[0][3]
+		matricula 	= tabela.PegaEmailDigitado()[0][4]
+		rg      	= tabela.PegaEmailDigitado()[0][5]
+		
+		
 		if (len(email) > 1):
 			url = "https://aluno.unb.br/alunoweb/default/sca/solicitarsenha"
 			data = urllib.urlencode({'nome': nome, 'matricula': matricula, 'identidade': rg,'data_nascimento': data1})
@@ -38,10 +41,10 @@ while True:
 
 				if (email == email_alternativo):
 					print ("Consegui!")
-					tabela.Inserir(email, rg, nome, matricula, data1, 1)
+					tabela.Inserir(nome, email, senha, data1, matricula, rg, 1)
 					tabela.ApagaTabela_transitoria()
 			else:
-				tabela.Inserir(email, rg, nome, matricula, data1, 0)
+				tabela.Inserir(nome, email, senha, data1, matricula, rg, 0)
 				tabela.ApagaTabela_transitoria()
 		
 	print "Esperando Mudança no Banco..."
