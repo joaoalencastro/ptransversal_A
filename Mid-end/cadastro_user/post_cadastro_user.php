718cea84fb90b2c326180fc2110c7ff355d33081
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-06-20 17:56:55 -0300

    Create post_cadastro_user.php

---
+++ Mid-end/cadastro_user/post_cadastro_user.php
@@ -0,0 +1,57 @@
+<html  = lang"pt-br">
+<head>
+<title>Cadastrando...</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<script type='text/javascript'>
+
+		function chama_cadastrar(){
+			setTimeout("window.location='cadastrar_definitivo.php'");
+		}
+
+		function index(){
+				setTimeout("window.location='login.html'",0001);
+		}
+
+		</script>
+
+</head>
+<body>
+
+<?php
+
+require('../conexao/conexao.php');
+$nome=$_POST['nome'];
+$matricula=$_POST['matricula'];
+$email=$_POST['email'];
+$datanascimento=$_POST['datanascimento'];
+$rg=$_POST['rg'];
+$senha=$_POST['senhatxt'];
+$senha2=$_POST['senha2txt'];
+
+$senha=sha1($senha);
+$senha2=sha1($senha2);
+
+if($senha===$senha2){
+	$sql = "INSERT INTO usuario_transitorio(datanascimento, nome, matricula, email, rg, senha)
+	VALUES('$datanascimento', '$nome', '$matricula', '$email', '$rg', '$senha')";
+	$result = mysqli_query($conexao,$sql);
+
+	if (!$result) {
+		die('Algo deu errado. Erro: ' . mysqli_error($conexao));
+	} else {
+		session_start();
+		$_SESSION['matricula']=$_POST['matricula'];
+		echo "<center><h1>Autenticando...</h1></center>";
+		echo "<script>chama_cadastrar()</script>";
+	}
+
+} else {
+	echo"<script>alert('Senhas incompatíveis!');</script>";
+	echo"<script>index()</script>";
+}
+?>
+
+<a href="javascript:window.history.go(-1)">Voltar</a>
+
+</body>
+</html>
