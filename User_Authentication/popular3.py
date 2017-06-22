2c353891bfd8ae03fdc0ec8d34cf8c7079639261
joaoalencastro <joao.alencastro@uiot.org>
2017-06-22 15:17:31 -0300

    alteracoes no executavel authentication

--- User_Authentication/popular3.py
+++ User_Authentication/popular3.py
@@ -6,7 +6,7 @@ sys.setdefaultencoding("utf-8")
 
 class Banco(object):
 	def __init__(self):
-		self.con  = MySQLdb.connect(host='localhost', user='root', passwd='***',db='sistemareservadodb')
+		self.con  = MySQLdb.connect(host='localhost', user='root', passwd='grupoass123',db='sistemareservadodb')
 		self.c = self.con.cursor()
 		
 
