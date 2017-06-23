8250ba632102a27f1fc3cbdc43a3ce72ad71acdf
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-06-23 01:20:10 -0300

    Update index.php

--- Server/prof_logado/index.php
+++ Server/prof_logado/index.php
@@ -85,7 +85,7 @@
                             <li class="dropdown">
 
                                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                                   <i class="fa fa-user fa-fw"></i><?php $nome  ?><i class="fa fa-caret-down"></i>
+                                   <i class="fa fa-user fa-fw"></i><?php echo $nome;  ?><i class="fa fa-caret-down"></i>
                                 </a>
                                 <ul class="dropdown-menu dropdown-user">
                                     <li>
