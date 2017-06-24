8f262716421580c40b0f04d73a90d0e99c409132
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-06-24 17:37:58 -0300

    Create post_solicitacao.php

---
+++ Server/solicitacao/post_solicitacao.php
@@ -0,0 +1,47 @@
+<?php
+
+require('../conexao/conexao.php');
+session_start();
+
+date_default_timezone_get("America/Sao_Paulo");
+
+$dados_solicitacao=$_POST['dados'];
+$horario_da_reserva=dados_solicitacao[0];
+$motivo=dados_solicitacao[1];
+$nomesala=dados_solicitacao[2];
+$horario_solicitacao_eua = date('Y-m-d H:i:s');
+$horario_solicitacao = date('d/m/Y H:i:s',  strtotime($horario_solicitacao_eua));
+$solicitante = $_SESSION['nome'];
+$email = $_SESSION['email'];
+$senha = $_SESSION['senha'];
+$id_usuario = $_SESSION['id'];
+
+$sql_achar_sala = "SELECT * FROM salas WHERE nome=$nomesala";
+
+$result = mysqli_query($conexao,$sql_achar_sala);
+if (!$result) {
+  die('Algo deu errado na conexão para encontrar sala. Erro: ' . mysqli_error($conexao));
+}
+
+while ($ln = mysqli_fetch_array($result)){
+  $id_sala = $ln['id'];
+}
+
+$sql_inserir_sol =  "INSERT INTO solicitacao(solicitante, horario_da_reserva, horario_solicitacao, id_usuario, motivo, nomesala)
+VALUES('$solicitante', '$horario_da_reserva', '$horario_solicitacao', '$id_usuario', '$motivo', '$nomesala')";
+
+$upload_solicitacao = mysqli_query($conexao,$sql_inserir_sol);
+
+if (!$upload_solicitacao) {
+  die('Algo deu errado na conexão para upar a solicitação. Erro: ' . mysqli_error($conexao));
+}
+
+$sql_status = "UPDATE fluxo_de_dados_sala set status='pendente' where id=$id_sala";
+
+$upload_solicitacao = mysqli_query($conexao,$sql_status);
+if (!$upload_solicitacao) {
+  die('Algo deu errado na conexão para mudar o status. Erro: ' . mysqli_error($conexao));
+} else {
+  echo"Solicitação Enviada à secertária com sucesso!";
+}
+?>
