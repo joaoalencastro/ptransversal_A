fe875691fc296e45197a19ed61c065fe2f32e857
rodrigojhoa <rodrigojhoa@hotmail.com>
2017-06-24 15:25:20 -0300

    Add files via upload

---
+++ Server/materiasphp/materias.php
@@ -0,0 +1,22 @@
+<?php
+include('conexao.php');
+$result = mysqli_query($conexao, "select * from materias");
+while ($r = mysqli_fetch_assoc($result)) {
+    $nome[] = $r['nome'];
+    $vagas[] = $r['vagas'];
+    $professor[] = $r['professor'];
+    $codigo[] = $r['codigo'];
+    $dias[] = $r['dias'];
+    $local[] = $r['locais'];
+    $horario[] = $r['horario'];
+
+}
+$json[0] = $nome;
+$json[1] = $vagas;
+$json[2] = $professor;
+$json[3] = $codigo;
+$json[4] = $dias;
+$json[5] = $local;
+$json[6] = $horario;
+echo json_encode($json);
+?>
