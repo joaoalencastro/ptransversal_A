e8812ee93643b0b9614f60e92a90d89fbbc75a26
rodrigojhoa <rodrigojhoa@hotmail.com>
2017-06-24 15:02:26 -0300

    Update conexao.php

--- Front-end/homePage/php/conexao.php
+++ Front-end/homePage/php/conexao.php
@@ -1,10 +1,9 @@
 <?php
   $host = "localhost";
   $user = "root";
-  $pass = "cebola123";
+  $pass = "";
   $db = "sistemareservadodb";
   $conexao = new mysqli ($host, $user, $pass, $db);
-
   // Caso algo tenha dado errado, exibe uma mensagem de erro
   if (mysqli_connect_errno()) {
       die('Não foi possível conectar-se ao banco de dados: ' . mysqli_connect_error());
