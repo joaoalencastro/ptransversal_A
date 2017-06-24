9782d87812a92d779ef730556f00f76d275347b6
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-06-24 19:19:44 -0300

    Update and rename index.html to index.php

--- Server/func/index.php
+++ Server/func/index.php
@@ -1,3 +1,17 @@
+<?php
+  require('../../conexao/conexao.php');
+  session_start();
+  if(!isset($_SESSION['email']) || !isset($_SESSION['senha'])){
+    header("Location: login.html");
+    exit;
+  } else {
+    $nome  =  $_SESSION['nome'];
+    $email =  $_SESSION['email'];
+    $senha =  $_SESSION['senha'];
+    echo "<h2>$nome</h2> "."Você está logado com o E-mail: "."$email "."e senha:  "."$senha";
+  }
+?>
+
 <!DOCTYPE html>
 <html>
 
