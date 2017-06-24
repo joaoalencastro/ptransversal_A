5c2ea52815ffadfc315ee627d758b3adac860b72
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-06-24 19:20:40 -0300

    Update index.php

--- Server/func/index.php
+++ Server/func/index.php
@@ -8,7 +8,6 @@
     $nome  =  $_SESSION['nome'];
     $email =  $_SESSION['email'];
     $senha =  $_SESSION['senha'];
-    echo "<h2>$nome</h2> "."Você está logado com o E-mail: "."$email "."e senha:  "."$senha";
   }
 ?>
 
