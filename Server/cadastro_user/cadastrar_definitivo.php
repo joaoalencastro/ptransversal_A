deafd88d527e3c79b6e4178b7cc3e45c944b9ee5
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-06-25 16:54:29 -0300

    Update cadastrar_definitivo.php

--- Server/cadastro_user/cadastrar_definitivo.php
+++ Server/cadastro_user/cadastrar_definitivo.php
@@ -1,25 +1,25 @@
 <?php
-
 session_start();
-
 require('../conexao/conexao.php');
 
 $matricula=$_SESSION['matricula'];
+
 echo"$matricula";
 
-$sql = "SELECT * FROM usuariodef WHERE matricula='$matricula'";	
+$sql = "SELECT * FROM usuariodef WHERE matricula='$matricula'";
 
 $result = mysqli_query($conexao,$sql);
+
 if (!$result) {
   die('Algo deu errado na conexão. Erro: ' . mysqli_error($conexao));
 }
-
 $row = mysqli_num_rows($result);
-echo"$row";
 
-if($row == 1){ 
+echo"linhas="."$row";
+
+if($row == 1){
+
 	while ($ln = mysqli_fetch_array($result)){
-
 	$nome      = $ln['nome'];
 	$email     = $ln['email'];
 	$matricula = $ln['matricula'];
@@ -29,22 +29,32 @@ if($row == 1){
 	$senha     = $ln['senha'];
 	$autenticado = $ln['autenticado'];
 	}
-	
-	if ($autenticado == 2){
-	echo"Já cadastrado!";
-		
-	} else if ($autenticado == 1) {
-	$sql = "INSERT INTO usuario(nome, email, matricula, data_nascimento, tipo, rg, senha)
-	VALUES('$nome', '$email', '$matricula', '$datanascimento', '$tipo', '$rg', '$senha')";
-	$result1 = mysqli_query($conexao,$sql);
 
-	      if (!$result1) {
-		die('Algo deu errado. Erro: ' . mysqli_error($conexao));
+	if ($autenticado == 1) {
+
+		$sql = "INSERT INTO usuario(nome, email, matricula, datanascimento, tipo, rg, senha)
+		VALUES('$nome', '$email', '$matricula', '$datanascimento', '$tipo', '$rg', '$senha')";
+
+		$result1 = mysqli_query($conexao,$sql);
+
+		  if (!$result1) {
+			die('Algo deu errado. Erro: ' . mysqli_error($conexao));
+
+			} else {
+			session_destroy();
+			echo"Cadastrado!";
+
+			$sql_delete = "TRUNCATE usuariodef";
+			$result_delete = mysqli_query($conexao,$sql_delete);
+			}
 		} else {
-		session_destroy();
-		echo"Cadastrado!";
-		}	
+			 echo"Dados inválidos!";
+		}
+
+	}else if($row > 1) {
+	    	echo"Usuário já cadastrado!";
+
 	} else {
-	    	echo"Dados invalidos!";
-	}
+				echo"Tente cadastrar novamente!";
+}
 ?>
