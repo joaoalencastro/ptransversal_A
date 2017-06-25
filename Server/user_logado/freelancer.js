c838bf2b303a601339357e45db1a54d70ca6d3ac
rodrigojhoa <rodrigojhoa@hotmail.com>
2017-06-25 19:43:26 -0300

    Update freelancer.js

--- Server/user_logado/freelancer.js
+++ Server/user_logado/freelancer.js
@@ -265,8 +265,27 @@ function clickSolic(id_str)
 
     $.ajax({
         type: "POST",
-        url: "../solicitacao/post_solicitacao.php",
-        data: "dados="+ dados_solicitacao,
+        url: "././solicitacao/post_solicitacao.php",
+        data: "dados=" + dados_solicitacao,
+        error: function (jqXHR, exception) {
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Not connect.\n Verify Network.';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+            console.log(msg);
+        }
     });
 }
 function init()
