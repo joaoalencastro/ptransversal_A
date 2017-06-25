73e05ec18f9501cd42c87567db244403f5b6f8bd
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-06-25 15:43:21 -0300

    Update cadastrar_definitivo.php

--- Server/cadastro_user/cadastrar_definitivo.php
+++ Server/cadastro_user/cadastrar_definitivo.php
@@ -6,41 +6,45 @@ require('../conexao/conexao.php');
 
 $matricula=$_SESSION['matricula'];
 
-$sql = "SELECT * FROM usuariodef WHERE matricula=$matricula";	
+$sql = "SELECT * FROM usuariodef WHERE matricula='$matricula'";	
 
 $result = mysqli_query($conexao,$sql);
 if (!$result) {
   die('Algo deu errado na conexão. Erro: ' . mysqli_error($conexao));
 }
 
-while ($ln = mysqli_fetch_array($result)){
+$row = mysqli_num_rows($result);
 
-$nome      = $ln['nome'];
-$email     = $ln['email'];
-$matricula = $ln['matricula'];
-$datanascimento = $ln['datanascimento'];
-$tipo = 1;
-$rg        = $ln['rg'];
-$senha     = $ln['senha'];
-$autenticado = $ln['autenticado'];
-}
-if ($autenticado == 0) {
-	echo"Dados invalidos!";
+if($row == 1){ 
+	while ($ln = mysqli_fetch_array($result)){
+
+	$nome      = $ln['nome'];
+	$email     = $ln['email'];
+	$matricula = $ln['matricula'];
+	$datanascimento = $ln['datanascimento'];
+	$tipo = 1;
+	$rg        = $ln['rg'];
+	$senha     = $ln['senha'];
+	$autenticado = $ln['autenticado'];
+	}
 	
-} else if ($autenticado == 2){
+	if ($autenticado == 2){
 	echo"Já cadastrado!";
-	
-} else if ($autenticado == 1) {
-  	$sql = "INSERT INTO usuario(nome, email, matricula, data_nascimento, tipo, rg, senha)
-  	VALUES('$nome', '$email', '$matricula', '$datanascimento', '$tipo', '$rg', '$senha')";
-  	$result1 = mysqli_query($conexao,$sql);
+		
+	} else if ($autenticado == 1) {
+	$sql = "INSERT INTO usuario(nome, email, matricula, data_nascimento, tipo, rg, senha)
+	VALUES('$nome', '$email', '$matricula', '$datanascimento', '$tipo', '$rg', '$senha')";
+	$result1 = mysqli_query($conexao,$sql);
 
-    if (!$result1) {
-  		die('Algo deu errado. Erro: ' . mysqli_error($conexao));
-  	} else {
-	session_destroy();
-	echo"Cadastrado!";
-        }
+		if (!$result1) {
+			die('Algo deu errado. Erro: ' . mysqli_error($conexao));
+		} else {
+		session_destroy();
+		echo"Cadastrado!";
+		}	
+} else {
+    echo"Dados invalidos!";
+}
 }	
 
 ?>
