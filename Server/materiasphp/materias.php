d2ff3925886130e7b4803248fc8c64416e781fa7
rodrigojhoa <rodrigojhoa@hotmail.com>
2017-06-25 17:35:05 -0300

    Update materias.php

--- Server/materiasphp/materias.php
+++ Server/materiasphp/materias.php
@@ -1,5 +1,5 @@
 <?php
-require('././conexao/conexao.php');
+require('../conexao/conexao.php');
 $result = mysqli_query($conexao, "select * from materias");
 while ($r = mysqli_fetch_assoc($result)) {
     $nome[] = $r['nome'];
