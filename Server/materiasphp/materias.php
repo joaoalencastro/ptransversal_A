5d158bf8ade3b5e7de95ca6943f274a8baf453f5
rodrigojhoa <rodrigojhoa@hotmail.com>
2017-06-25 22:42:49 -0300

    Update materias.php

--- Server/materiasphp/materias.php
+++ Server/materiasphp/materias.php
@@ -2,18 +2,16 @@
 require('../conexao/conexao.php');
 $sql = "SELECT * FROM materias";
 $result = mysqli_query($conexao, $sql);
-/*
-while($r = mysqli_fetch_array($result))
-{
-    $nome[] = $r['nome']; 
-    $vagas[] = $r['vagas'];
-    $professor[] = $r['professor'];
-    $codigo[] = $r['codigo'];
-    $dias[] = $r['dias'];
-    $local[] = $r['locais'];
-    $horario[] = $r['horario'];
 
-}
+$r = mysqli_fetch_array($result)
+    $nome = $r['nome']; 
+    $vagas = $r['vagas'];
+    $professor = $r['professor'];
+    $codigo = $r['codigo'];
+    $dias = $r['dias'];
+    $local = $r['locais'];
+    $horario = $r['horario'];
+
 $json[0] = $nome;
 $json[1] = $vagas;
 $json[2] = $professor;
@@ -22,9 +20,5 @@ $json[4] = $dias;
 $json[5] = $local;
 $json[6] = $horario;
 echo json_encode($json);
-*/
-$r = mysqli_fetch_array($result);
-    $test[0]= $r['nome']; 
-    $test[1] = $r['vagas'];
-    echo function_exists ('json_encode');   
+
 ?>    
