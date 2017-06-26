2c5c3d1cf718eaf57e74fa6384f8de30c5412837
rodrigojhoa <rodrigojhoa@hotmail.com>
2017-06-26 00:15:28 -0300

    Update materias.php

--- Server/materiasphp/materias.php
+++ Server/materiasphp/materias.php
@@ -1,5 +1,4 @@
 <?php
-/*
 require('../conexao/conexao.php');
 $sql = "SELECT * FROM materias";
 $result = mysqli_query($conexao, $sql);
@@ -7,7 +6,6 @@ $result = mysqli_query($conexao, $sql);
 while($r = mysqli_fetch_array($result)){
     $nome[] = $r['nome']; 
 }
-*/
-$test = array('rodrgo','jhoa','sasa','sasa');
-echo json_encode($test);
+$json[0] = $nome;
+echo json_encode($json);
 ?>    
