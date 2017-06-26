30689e98cc92f71d3d7c9cfc01c6612d12d60d74
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-06-26 01:14:39 -0300

    Update post_solicitacao.php

--- Server/solicitacao/post_solicitacao.php
+++ Server/solicitacao/post_solicitacao.php
@@ -36,26 +36,25 @@ $result_achar_sala = mysqli_query($conexao,$sql_achar_sala_fluxo);
 $row_sala = mysqli_num_rows($result_achar_sala);
 
 if ($row_sala == 1) {
-  
+
     $sql_status = "UPDATE fluxo_sala SET status_sala='pendente' WHERE id='$id_sala'";
     $upload_status_fluxo = mysqli_query($conexao,$sql_status);
     if (!$upload_status_fluxo) {
       die('Algo deu errado na conexão para alterar o status. Erro: ' . mysqli_error($conexao));
     }
 
-    $sql_inserir_sol =  "INSERT INTO solicitacao(nomesala, solicitante, horario_da_reserva, horario_solicitacao, id_usuario, motivo)
-    VALUES('$nomesala', '$solicitante', '$horario_da_reserva', '$horario_solicitacao', '$id_usuario', '$motivo')";
+    $sql_inserir_sol =  "INSERT INTO solicitacao(solicitante, horario_da_reserva, horario_solicitacao, id_usuario, motivo, nomesala)
+    VALUES('$solicitante', '$horario_da_reserva', '$horario_solicitacao', '$id_usuario', '$motivo', '$nomesala')";
 
     $upload_solicitacao = mysqli_query($conexao,$sql_inserir_sol);
     if (!$upload_solicitacao) {
       die('Algo deu errado na conexão para upar a solicitação. Erro: ' . mysqli_error($conexao));
 
     }
-    echo"Solicitação enviada à secretária com sucesso!";
+    echo"Solicitação Enviada à secretária com sucesso!";
 
 } else {
   echo"ID não encontrado na tabela fluxo_de_dados_sala";
-  echo"$id_sala";
 }
 
 ?>
