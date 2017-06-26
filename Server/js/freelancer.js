1584961ac0642ee7e46ff842b6b0981a373f9fda
rodrigojhoa <rodrigojhoa@hotmail.com>
2017-06-26 00:06:11 -0300

    Update freelancer.js

--- Server/js/freelancer.js
+++ Server/js/freelancer.js
@@ -251,6 +251,7 @@ function Getmateriasfromserver(callback){
     //Requisição HTTP, por dados provindos do url dado. Caso os dados recebidos sejam os esperados, entra no caso do SUCCESS
     return $.ajax({
         url: 'materiasphp/materias.php',
+	dateType: 'json',
         success: function(data)
         {
             console.log(data);
