f17025a223c0bf1770ee49cbee0ef52ad3107789
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-06-26 02:01:26 -0300

    Update post_solicitacao.php

--- Server/solicitacao/post_solicitacao.php
+++ Server/solicitacao/post_solicitacao.php
@@ -51,8 +51,8 @@ if ($row_sala == 1) {
 
     }
     echo"Solicitação Enviada à secretária com sucesso! Aguarde para ser redirecionado!";
-    sleep(4);
-	 header('Location: ../index.php');
+    	sleep(4);
+	 header('Location: ../user_logado/index.php');
 
 } else {
   echo"ID não encontrado na tabela fluxo_de_dados_sala";
