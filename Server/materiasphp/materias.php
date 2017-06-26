45e7bead7f86cd862dedadde31ba487740636acb
rodrigojhoa <rodrigojhoa@hotmail.com>
2017-06-26 00:48:24 -0300

    Update materias.php

--- Server/materiasphp/materias.php
+++ Server/materiasphp/materias.php
@@ -4,8 +4,19 @@ $sql = "SELECT * FROM materias";
 $result = mysqli_query($conexao, $sql);
 
 while($r = mysqli_fetch_array($result)){
-    $nome[] = $r['nome']; 
+     $vagas[] = $r['vagas'];
+     $professor[] = $r['professor'];   
+     $codigo[] = $r['codigo'];
+     $dias[] = $r['dias'];
+     $local[] = $r['locais'];
+     $horario[] = $r['horario'];
 }
-$json[0] = $nome;
+ $json[0] = $nome;
+ $json[1] = $vagas;
+ $json[2] = $professor;
+ $json[3] = $codigo;
+ $json[4] = $dias;
+ $json[5] = $local;
+ $json[6] = $horario;
 echo json_encode($json);
 ?>    
