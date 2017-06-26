6e586e28a40db29bcb2ee20576b4faafd46e5d11
lucasgarciareis <lucasreis.lgrf@gmail.com>
2017-06-26 02:54:44 -0300

    Update post_solicitacao.php

--- Server/solicitacao/post_solicitacao.php
+++ Server/solicitacao/post_solicitacao.php
@@ -50,7 +50,7 @@ if ($row_sala == 1) {
       die('Algo deu errado na conexão para upar a solicitação. Erro: ' . mysqli_error($conexao));
 
     }
-   echo "<script>alert('Email enviado com Sucesso!);location.href=\"../user_logado/index.php\";</script>";
+  header("location:javascript:alert(\"Solicitacao enviada com sucesso!\");location.href=\"../user_logado/index.php\";");
 
 } else {
   echo"ID não encontrado na tabela fluxo_de_dados_sala";
