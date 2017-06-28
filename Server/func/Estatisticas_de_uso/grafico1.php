b9e3ad220356bf31298f3509568248ff2d951f3c
Bruno Vieira Dutra <bruno.dutra@uiot.org>
2017-06-28 18:40:57 -0300

    Update grafico1.php

--- Server/func/Estatisticas_de_uso/grafico1.php
+++ Server/func/Estatisticas_de_uso/grafico1.php
@@ -12,7 +12,7 @@ $grafico->SetTitle("Dados Interessantes");
 
 
 
-$sql2 = "SELECT * FROM fluxo;";
+$sql2 = "SELECT * FROM fluxo_sala;";
 $result2 = mysqli_query($conexao,$sql2);
 if (!$result2) {
 	die('Algo deu errado na conexão. Erro: ' . mysqli_error($conexao));
@@ -24,7 +24,7 @@ while ($ln2 = mysqli_fetch_array($result2)){
 }
 
 
-$sql3 = "SELECT * FROM fluxo WHERE status = 'pendente';";
+$sql3 = "SELECT * FROM fluxo_sala WHERE status = 'pendente';";
 $result3 = mysqli_query($conexao,$sql3);
 if (!$result3) {
 	die('Algo deu errado na conexão. Erro: ' . mysqli_error($conexao));
