d0e2df392ba732127678b7719dd779bb6e46e993
Lucas Vinicius <lcsvncs429@gmail.com>
2017-06-28 17:51:08 -0300

    Update freelancer.js

--- Server/js/freelancer.js
+++ Server/js/freelancer.js
@@ -265,6 +265,18 @@ function init()
             endDate : '+15d',
             daysOfWeekDisabled: [0]
 
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });	
+		
+		
+		
         }).on('changeDate', function (e) {
         setDate();
         });
