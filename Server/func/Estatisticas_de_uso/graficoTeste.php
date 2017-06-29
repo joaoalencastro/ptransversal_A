d4302f6c053ea130ff8898778584827e703f1790
Bruno Vieira Dutra <bruno.dutra@uiot.org>
2017-06-28 22:37:21 -0300

    Update graficoTeste.php

--- Server/func/Estatisticas_de_uso/graficoTeste.php
+++ Server/func/Estatisticas_de_uso/graficoTeste.php
@@ -25,11 +25,11 @@ $MeuGrafico = new PHPlot(400, 200);
 $MeuGrafico->SetFileFormat("png");
 
 // define o tipo de grafico, nesse caso em barras
-$MeuGrafico->SetPlotType('bars');
+$MeuGrafico->SetPlotType("bars");
 
 // define se as barras serão em 3D, valor 0 Imagem chamada
 $MeuGrafico->SetShading(1); 
-$MeuGrafico->SetDataType('text-data');
+$MeuGrafico->SetDataType("text-data");
 
 
 //função para converter para real, pode haver outra maneira!
@@ -43,24 +43,24 @@ return "R$ {$real}";
 
 // setar o valores no eixo Y no formato moeda
 // este metodo aceita uma função quando o parametro custom é setado
-$MeuGrafico->SetYLabelType('custom', 'converterParaReal');
+$MeuGrafico->SetYLabelType("custom", "converterParaReal");
 
 // seta os dados para a plotagem do grafico
 $MeuGrafico->SetDataValues($dados);
 
 // definição do titulo do gráfico
 // por questão da acentuação utilizar a função utf8_decode
-$titulo = utf8_decode('DEMONSTRAÇÃO DO RESULTADO DE VENDAS BIMENTRAL');
+$titulo = utf8_decode("DEMONSTRAÇÃO DO RESULTADO DE VENDAS BIMENTRAL");
 
 
 // Setar o titulo definido na varieavel $titulo anteriomente
 $MeuGrafico->SetTitle($titulo);
 
 // Gera uma legenda
-$MeuGrafico->SetLegend(array('Pedro', 'Paulo'));
+$MeuGrafico->SetLegend(array("Pedro", "Paulo"));
 
 //Por padrão é setado "marcas" das escalas do eixo x, none retira estas marcas.
-$MeuGrafico->SetXTickPos('none');
+$MeuGrafico->SetXTickPos("none");
 
 // gera o grafico
 $MeuGrafico->DrawGraph();
