19f9118ad4ada8828cca73bfbc05644ea0614c5f
rodrigojhoa <rodrigojhoa@hotmail.com>
2017-06-29 06:09:09 -0300

    Update addRoom.php

--- Server/func/tarefas_funcionario/addRoom.php
+++ Server/func/tarefas_funcionario/addRoom.php
@@ -9,7 +9,7 @@ $laboratorio = $_POST['laboratorio'];
 
 $temp = explode(".", $_FILES["file"]["name"]);
 $newfilename = round(microtime(true)) . '.' . end($temp);
-move_uploaded_file($_FILES["file"]["tmp_name"], "../paginaInicialdeFuncionario/Imagens/" . $newfilename);
+move_uploaded_file($_FILES["file"]["tmp_name"], "../../img" . $newfilename);
 
 
 $sql = "INSERT INTO salas (ID,nome,responsavel,num_cadeiras,projetor,ar_condicionado,lab,idfluxo)
