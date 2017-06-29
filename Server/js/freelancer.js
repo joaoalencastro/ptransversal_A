b1d144411547fb88ad7002e9c7be6d1e64ace7ac
rodrigojhoa <rodrigojhoa@hotmail.com>
2017-06-29 04:58:49 -0300

    Update freelancer.js

--- Server/js/freelancer.js
+++ Server/js/freelancer.js
@@ -975,7 +975,7 @@ function getroomfromserver(callback){
     var status_aux;
     //Requisição HTTP, por dados provindos do url dado. Caso os dados recebidos sejam os esperados, entra no caso do SUCCESS
     return $.ajax({
-        url: 'materiasphp/salas.php',
+        url: 'materiasphp/nomesalas.php',
         dataType: 'json',
         success: function(data)
         {
