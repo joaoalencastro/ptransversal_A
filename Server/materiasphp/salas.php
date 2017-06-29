8e40993b8eb2a4e2035ed95d6144e5f7134113be
rodrigojhoa <rodrigojhoa@hotmail.com>
2017-06-29 04:43:58 -0300

    Add files via upload

--- Server/materiasphp/salas.php
+++ Server/materiasphp/salas.php
@@ -1,12 +1,18 @@
 <?php
 require('../conexao/conexao.php');
-$sql = "SELECT * FROM salas2";
+$sql = "SELECT * FROM fluxo_sala";
 $result = mysqli_query($conexao, $sql);
 while($r = mysqli_fetch_array($result))
 {
+    $stat[]= $r['status_sala'];
     $nome[] = $r['nome'];
+    $data[] = $r['data'];
+    $dias[] = $r['dias'];
 }
 $json[0] = $nome;
+$json[1] = $dias;
+$json[2] = $data;
+$json[3] = $stat;
 echo json_encode($json);
 
 ?>
