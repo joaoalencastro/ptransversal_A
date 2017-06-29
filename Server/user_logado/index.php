dba5640f336709d7b87c33476e0f6276f913f17a
aledias27 <alessandrofdj2015@gmail.com>
2017-06-29 02:11:39 -0300

    teste

--- Server/user_logado/index.php
+++ Server/user_logado/index.php
@@ -313,12 +313,12 @@
                             <div id="over" >
                             <img src="../img/mapaFT.png" style="width: 900px; height: 650px" alt="" usemap="#mymap"  border="0" >
                             <map name="mymap">
-                                <area shape="rect" coords="654,453,696,409" onclick="fecharmodal(this.id)" id="BT-52/15">
-                                <area shape="rect" coords="609,475,645,427" onclick="fecharmodal(this.id)" id="BT-34/15">
-                                <area shape="rect" coords="548,447,600,484" onclick="fecharmodal(this.id)" id="BT-25/15">
-                                <area shape="rect" coords="500,469,553,498" onclick="fecharmodal(this.id)" id="BT-16/15">
-                                <area shape="rect" coords="432,488,502,540" onclick="fecharmodal(this.id)" id="Auditório">
-                                <area shape="rect" coords="222,431,285,463" onclick="fecharmodal(this.id)" id="Lab-Redes">
+                                <area shape="rect" coords="654,453,696,409" onclick='$('#portifolioModal1').modal('hide');' data-toggle="modal" href="#BT-52_15M" id="BT-52/15">
+                                <area shape="rect" coords="609,475,645,427" onclick='$('#portifolioModal1').modal('hide');' data-toggle="modal" href="#BT-34_15M" id="BT-34/15">
+                                <area shape="rect" coords="548,447,600,484" onclick='$('#portifolioModal1').modal('hide');' data-toggle="modal" href="#BT-25_15M" id="BT-25/15">
+                                <area shape="rect" coords="500,469,553,498" onclick='$("Fechar").click()' data-toggle="modal" href="#BT-16_15M" id="BT-16/15">
+                                <area shape="rect" coords="432,488,502,540" onclick='$("Fechar").click()' data-toggle="modal" href="#AuditorioM" id="Auditório"> 
+                                <area shape="rect" coords="222,431,285,463" onclick='$("Fechar").click()' data-toggle="modal" href="#LabRedesM" id="Lab-Redes">	
                             </map>
                             </div>
                             <p>Clique em alguma sala para ver o seu itinerário.</p>
