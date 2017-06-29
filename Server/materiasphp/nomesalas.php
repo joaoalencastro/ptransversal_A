c726450c70a817244a8d026b56d39575462fbbb5
rodrigojhoa <rodrigojhoa@hotmail.com>
2017-06-29 04:47:15 -0300

    Create nomesalas.php

---
+++ Server/materiasphp/nomesalas.php
@@ -0,0 +1,11 @@
+<?php
+require('../conexao/conexao.php');
+$sql = "SELECT * FROM salas";
+$result = mysqli_query($conexao, $sql);
+while($r = mysqli_fetch_array($result))
+{	
+  $nome[] = $r['nome'];
+}
+$json[0] = $nome;
+echo json_encode($json);  		  
+?>
