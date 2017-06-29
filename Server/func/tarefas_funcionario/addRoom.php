3e8eb6edd1257e3ae15effe770e33e5490b5eae6
rodrigojhoa <rodrigojhoa@hotmail.com>
2017-06-29 05:53:00 -0300

    Add files via upload

--- Server/func/tarefas_funcionario/addRoom.php
+++ Server/func/tarefas_funcionario/addRoom.php
@@ -1,13 +1,19 @@
 <?php
-require('../../conexao/conexao.php');
+include('conexao.php');
+
 $nome = $_POST['nome'];
 $cadeiras = $_POST['cadeiras'];
 $projetor = $_POST['ar'];
 $ar = $_POST['projetor'];
 $laboratorio = $_POST['laboratorio'];
 
+$temp = explode(".", $_FILES["file"]["name"]);
+$newfilename = round(microtime(true)) . '.' . end($temp);
+move_uploaded_file($_FILES["file"]["tmp_name"], "../paginaInicialdeFuncionario/Imagens/" . $newfilename);
+
+
 $sql = "INSERT INTO salas (ID,nome,responsavel,num_cadeiras,projetor,ar_condicionado,lab,idfluxo)
-VALUES (NULL, '$nome', NULL, '$cadeiras', '$projetor', '$ar', '$laboratorio', NULL )";
+VALUES (NULL, '$nome', NULL, '$cadeiras', '$projetor', '$ar', '$laboratorio', NULL)";
 if ($conexao->query($sql) === TRUE) {
     echo "Sala criada com sucesso";
 } else {
